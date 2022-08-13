37ef2a8279a4d16fea5a0ae171745f40e546a4d1
Leva <parimalleva@gmail.com>
2022-08-13 12:05:41 +0300

    1 of 5 stage completed

---
+++ Linear Equations Solver/task/src/solver/Main.java
@@ -0,0 +1,30 @@
+package solver;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
+        LinearEquation equation = new LinearEquation(a, b);
+        System.out.println(equation.solve());
+    }
+}
+
+
+class LinearEquation {
+    private double a;
+    private double b;
+
+    LinearEquation(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    double solve() {
+        return this.b / this.a;
+    }
+}
