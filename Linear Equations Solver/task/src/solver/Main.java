b61ba63da99a03fda39c1d80d50eb178258b13d2
Leva <parimalleva@gmail.com>
2022-08-13 12:10:18 +0300

    2 of 5 stages completed

--- Linear Equations Solver/task/src/solver/Main.java
+++ Linear Equations Solver/task/src/solver/Main.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double d = scanner.nextDouble();
+        double e = scanner.nextDouble();
+        double f = scanner.nextDouble();
 
-        LinearEquation equation = new LinearEquation(a, b);
-        System.out.println(equation.solve());
+        LinearEquation equation = new LinearEquation(a, b, c, d, e, f);
+        equation.solve();
     }
 }
 
@@ -18,13 +21,24 @@ public class Main {
 class LinearEquation {
     private double a;
     private double b;
+    private double c;
+    private double d;
+    private double e;
+    private double f;
 
-    LinearEquation(double a, double b) {
+    LinearEquation(double a, double b, double c, double d, double e, double f) {
         this.a = a;
         this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
     }
 
-    double solve() {
-        return this.b / this.a;
+    void solve() {
+        double y = (f - c * d / a) / (e - b * d / a);
+        double x = (c - b * y) / a;
+        System.out.println(x + " " + y);
     }
 }
+
